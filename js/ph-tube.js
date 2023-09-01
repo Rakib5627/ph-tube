ce8e3cb74d8d2e5f934f2086e62e979bb0bfdeed
Rakibul Islam <mdrakib.5627@gmail.com>
2023-09-01 11:57:52 +0600

    responsive

--- js/ph-tube.js
+++ js/ph-tube.js
@@ -34,43 +34,95 @@ const showCards = async(categoryId) => {
 
 
 const displayCards =(cards)=>{
-    console.log(cards);
+
+    
+    // console.log(Array.isArray(cards));
+
+
+    cards.sort(function (a, b) {
+        return parseFloat(b.others.views)- parseFloat(a.others.views)
+        });
+
+        console.log(cards);
 
     const cardsContainer = document.getElementById('cards-container');
-
-
+    const noData = document.getElementById('no-data');
     cardsContainer.textContent= '';
+    noData.textContent= '';
 
 
+    if(cards.length !== 0){
 
     cards.forEach(card =>{
         const div = document.createElement('div');
 
-        div.innerHTML = `
-        <div>
-            <div class="">
-                <img src="${card.thumbnail}" alt="" class="h-40 w-full" />
-                <div class="flex items-start mt-4">
-                  <div class="mr-3">
-                    <img src="${card.authors[0].profile_picture}" alt="" class="rounded-full h-8 w-8" />
-                  </div>
-                  <div>
-                    <h2 class="text-base font-medium">${card.title}</h2>
-                    <div class="flex text-sm">
-                        <p>${card.authors[0].profile_name}</p>
-                        <p>${card.authors[0].verified}</p>
-                    </div>
-                    <p class="text-xs">${card.others.views}</p>
-                  </div>
-                </div>
-              </div>
+        const verified = card.authors[0].verified;
 
-        </div>
+        if(verified===true){
+
+            div.innerHTML = `
+            <div>
+                <div class="">
+                    <img src="${card.thumbnail}" alt="" class="h-40 w-full" />
+                    <div class="flex items-start mt-4">
+                      <div class="mr-3">
+                        <img src="${card.authors[0].profile_picture}" alt="" class="rounded-full h-8 w-8" />
+                      </div>
+                      <div>
+                        <h2 class="text-base font-medium">${card.title}</h2>
+                        <div class="flex text-sm">
+                            <p>${card.authors[0].profile_name}</p>
+                            <img src="./images/verified.svg" alt="">
+                        </div>
+                        <p class="text-xs">${card.others.views}</p>
+                      </div>
+                    </div>
+                  </div>
+    
+            </div>           
+            `            
+        }
+        else{
+                div.innerHTML = `
+                <div>
+                    <div class="">
+                        <img src="${card.thumbnail}" alt="" class="h-40 w-full" />
+                        <div class="flex items-start mt-4">
+                          <div class="mr-3">
+                            <img src="${card.authors[0].profile_picture}" alt="" class="rounded-full h-8 w-8" />
+                          </div>
+                          <div>
+                            <h2 class="text-base font-medium">${card.title}</h2>
+                            <div class="flex text-sm">
+                                <p>${card.authors[0].profile_name}</p>
+                                
+                            </div>
+                            <p class="text-xs">${card.others.views}</p>
+                          </div>
+                        </div>
+                      </div>
         
-        `
+                </div>
+                
+                `             
+            }
+        // console.log(verified)
+        
+       
         cardsContainer.appendChild(div);
     })
+}
+ else{
+        const div = document.createElement('div');
+        div.innerHTML = `
+               <div class="text-center">
+                   <img src="./images/Icon.png" alt="" class="w-1/4 mx-auto">
+                    <p class="text-2xl text-red-500">Sorry! No data found.</p>
+               </div>
 
+        `
+        noData.appendChild(div);
+ }
 
 
 }
